1ffd91482fbb26bc8bd3c422cc8f9dc30a2ada7d
Tilinn <jm293229@gmail.com>
2024-11-02 21:39:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -55,13 +55,13 @@ async function start(file) {
   if (isRunning) return;
   isRunning = true;
 
-  say('The Mystic\nBot', {
+  say('STICH\nBot', {
     font: 'chrome',
     align: 'center',
     gradient: ['red', 'magenta'],
   });
 
-  say(`Bot creado por Bruno Sobrino`, {
+  say(`Bot creado por TILIN`, {
     font: 'console',
     align: 'center',
     gradient: ['red', 'magenta'],
